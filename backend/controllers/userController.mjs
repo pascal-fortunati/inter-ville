639f4ff6aad98fb5dd3835cb80d5dd11736d0334
Fraxoo <fraxo.officiel@gmail.com>
2025-12-09 16:18:37 +0100

    added function in controller

--- backend/controllers/userController.mjs
+++ backend/controllers/userController.mjs
@@ -1,10 +1,42 @@
 import { getAllUsers } from "../models/userModel.mjs";
 
-export function getUsers(req,res){
-    try{
+const sendErrors = (res, errors, status = 400) => {
+    return res.status(status).json({ errors });
+};
+
+function catchError(res, err) {
+    if (err.name === "SequelizeValidationError") {
+        const errors = err.errors.map((e) => ({
+            field: e.path,
+            message: e.message,
+        }));
+        return sendErrors(res, errors, 400);
+    }
+    return sendErrors(res, [{ field: "global", message: err.message }], 500);
+}
+
+
+export function getUsers(req, res) {
+    try {
         const users = getAllUsers();
         res.status(200).json(users)
+    } catch (err) {
+        return catchError(res,err)
+    }
+}
+
+export async function register(req,res) {
+    try{
+
     } catch(err){
-        return res.status(500).json(err);
+        return catchError(res,err)
+    }
+}
+
+export async function login(req,res) {
+    try{
+
+    } catch(err){
+        return catchError(res,err)
     }
 }
--- backend/models/userModel.mjs
+++ backend/models/userModel.mjs
@@ -1,12 +1,12 @@
 import db from "../config/database.mjs"
 
 
-export function createUser(){
-    return db.prepare("INSERT INTO users ()")
+export function createUser() {
+    return db.prepare("INSERT INTO customers (,) VALUES ()")
 }
 
 
 
-export function getAllUsers(){
+export function getAllUsers() {
     return db.prepare("SELECT * FROM users").all();
 }
