6ca7f9a79f30ad4bce94bc922af15758bb4486f6
Fraxoo <fraxo.officiel@gmail.com>
2025-12-12 16:52:36 +0100

    save

--- backend/models/messageModel.mjs
+++ backend/models/messageModel.mjs
@@ -1,1 +1,16 @@
-import db from "../config/database.mjs"
+import db from "../config/database.mjs";
+
+export function addMessage(userId, content, channelId) {
+    const query = "INSERT INTO messages (content, user_id,channel_id) VALUES (?,?,?)";
+    const stmt = db.prepare(query);
+    const result = stmt.run(content, userId, channelId)
+    return result.changes;
+}
+
+export function deleteMessage(userId, messageId, channelId) {
+
+};
+
+export function getAllMessagesByChannelId(channelId) {
+
+}
