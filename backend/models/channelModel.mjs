c5c69a28ad241aedec057d03a94814164629d8b0
Fraxoo <fraxo.officiel@gmail.com>
2025-12-12 14:21:43 +0100

    added a few function

--- backend/models/channelModel.mjs
+++ backend/models/channelModel.mjs
@@ -8,7 +8,7 @@ export function createChannel(name, ownerId, type) {
 }
 
 export function deleteChannel(id) {
-    const query = "DELETE FROM channels where id = ?";
+    const query = "DELETE FROM channels WHERE id = ?";
     const stmt = db.prepare(query);
     const result = stmt.run(id);
     return result.changes;
@@ -22,13 +22,21 @@ export function updateChannel(id, name, ownerId, type) {
 }
 
 export function addUserToChannel(userId, channelId) {
-
+    const query = "INSERT INTO channels_users (channel_id,user_id) VALUES (?,?)";
+    const stmt = db.prepare(query);
+    const result = stmt.run(channelId, userId);
+    return result;
 }
 
 export function removeUserFromChannel(userId, channelId) {
-
+    const query = "DELETE FROM channel_users WHERE user_id = ? AND channel_id = ?";
+    const stmt = db.prepare(query);
+    const result = stmt.run(userId, channelId)
 }
 
 export function changeUserRoleOnChannel(userId, channelId, role) {
-
+    const query = "UPDATE channels_users SET role = ? WHERE channel_id = ? AND user_id = ?";
+    const stmt = db.prepare(query);
+    const result = stmt.run(role, channelId, userId);
+    return result.changes;
 }
